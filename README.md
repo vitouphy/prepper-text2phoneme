1ac66b01d873f83abd95ef2bb2c390ae9c5ca8ef
Vitou Phy <phy_vitou@live.com>
2022-05-26 00:40:59 +0900

    add espeaker for text2phoneme converter

---
+++ README.md
@@ -0,0 +1,23 @@
+# Text2Phoneme
+
+Setup the library
+
+### On Debian/Unbuntu
+```
+sudo apt-get install festival espeak-ng mbrola
+```
+
+### On MacOS
+```
+brew install espeak
+```
+
+## Installation
+```
+pip install -r requirements.txt
+```
+
+## Request
+```
+http://localhost:8000/convert?text=hello%20world
+```
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from typing import Union
-
 from fastapi import FastAPI
+from phonemizer.backend import EspeakBackend
+from phonemizer.punctuation import Punctuation
+from phonemizer.separator import Separator
 
+# Initialize fastapi server
 app = FastAPI()
-
+# initialize the espeak backend for English
+backend = EspeakBackend('en-us')
+# separate phones by a space and ignoring words boundaries
+separator = Separator(phone=' ', word=None)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"text": "It's working"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/convert")
+def convert_text2phoneme(text: str):
+    text = Punctuation(';:,.!"?()-').remove(text)
+    phonemes = backend.phonemize(text.split(), separator=separator, strip=True)
+    phonemes = ["".join(phoneme.split()) for phoneme in phonemes]
+    result = " ".join(phonemes) 
+        
+    return {
+        "text": result,
+        "source_text": text
+    }
